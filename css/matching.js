916306396592f9e8aa8e8e9f08ada782ba810d7a
divyanshu <divyanshu.r46956@gmail.com>
2016-10-30 16:37:54 +0530

    Project Completed !

---
+++ css/matching.js
@@ -0,0 +1,45 @@
+ var Number_0f_faces=5;
+        var theLeftSide=document.getElementById("leftside");
+        var right_side=document.getElementById("rightside"); 
+        var thebody=document.getElementById("thebody");
+        function generateFaces()
+        {
+            console.log(Number_0f_faces);
+            var count=Number_0f_faces;
+            while(count > 0)
+            {
+                var new_element=document.createElement("img");
+                new_element.src="http://home.cse.ust.hk/~rossiter/mooc/matching_game/smile.png";
+                theLeftSide.appendChild(new_element);
+                x_position=Math.floor(Math.random()*400);
+                y_position=Math.floor(Math.random()*400);
+                new_element.style.left=x_position + "px";
+                new_element.style.top=y_position + "px";
+                count=count-1;
+            }   
+                var left_images=theLeftSide.cloneNode(true);
+                    left_images.removeChild(left_images.lastChild);
+                    right_side.appendChild(left_images);
+                    theLeftSide.lastChild.onclick=
+                    function nextLevel(event)
+                  {
+                    event.stopPropagation();
+                    while (theLeftSide.hasChildNodes()) 
+                          {   
+                              theLeftSide.removeChild(theLeftSide.firstChild);
+                          }
+                    while (right_side.hasChildNodes()) 
+                          {   
+                              right_side.removeChild(right_side.firstChild);
+                          }
+                            Number_0f_faces=Number_0f_faces+5;
+                            generateFaces();
+                            
+                  };
+                thebody.onclick = function gameOver() {
+                                                        alert("Game Over!");
+                                                        thebody.onclick = null; 
+                                                        theLeftSide.lastChild.onclick = null;
+            
+                };           
+        }
